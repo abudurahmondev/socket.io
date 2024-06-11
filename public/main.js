fd73550367f593d4e9bd868bb629e77e2ba2168e
abudurahmondev <k809292@gmail.com>
2024-06-11 12:28:22 +0500

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,45 @@
+const socket = io()
+
+const clientsTotal = document.getElementById('client-total')
+
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    sendMessage()
+})
+
+socket.on('clients-total', (data) => {
+    clientsTotal.innerText = `Members: ${data}`
+})
+
+function sendMessage() {
+    console.log(messageInput.value);
+    const data = {
+        name: nameInput.value,
+        message: messageInput.value,
+        dateTime: new Date()
+    }
+    socket.emit('message', data)
+    addMessageToUI(true, data)
+    messageInput.value = ''
+}
+
+socket.on('chat-message', (data) => {
+    addMessageToUI(false, data)
+})
+
+function addMessageToUI(isOwnMessage, data) {
+    const element = `
+    <li class="${isOwnMessage ? "message-right" : "message-left"}">
+    <p class="message">
+        ${data.message}
+        <span> ${data.name} | ${moment(data.dateTime).fromNow()}</span>
+    </p>
+    </li>
+    `
+    messageContainer.innerHTML += element
+}
